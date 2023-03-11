bc2f7ef4fded6bcbd3a053435f182de4e3a39e43
Jonah <34112011+jcortezzo@users.noreply.github.com>
2023-03-11 11:04:16 -0500

    Implement Ep.4 Rolling

---
+++ SoulsLike/Assets/Scripts/ResetIsInteracting.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ResetIsInteracting : StateMachineBehaviour
+{
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("IsInteracting", false);
+    }
+}
